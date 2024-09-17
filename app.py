13575aa909cab8d7c3dbb31dcbe0e5b1c7c1e6fc
AlegriaC <ale.carrion@hotmail.com>
2024-09-17 15:19:52 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+car_data = pd.read_csv(
+    'C:\\Users\\aleca\\OneDrive\\Data Analyst\\Projects\\Project 6\\Project6\\vehicles_us.csv')
+
+st.header('Venta de vehículos usados en USA')
+
+# crear un botón para la creación de un histograma
+hist_button = st.button('Construir histograma')
+
+if hist_button:  # al hacer clic en el botón se despliega un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches respecto al kilometraje')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# crear un boton para la creacion de un diagrama de dispersión
+scat_button = st.button('Contruir un diagrama de dispersion')
+
+if scat_button:
+    st.writte("Creación de un diagrama de dispersión entre kilometraje y precio")
+
+    # crear el diagrama de dispersión
+    fig2 = px.scatter(car_data, x='odometer', y='price',
+                      title='Kilimetraje vs Precio')
+
+    # mostar gráfico interactivo
+    st.plotly_chart(fig2, use_container_width=True)
